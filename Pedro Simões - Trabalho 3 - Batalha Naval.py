7dc3fad7ce31531dfb91dc60ab980123e6eea1fa
Pedro Simoes <176055646+404PeterSimoes@users.noreply.github.com>
2025-06-11 12:56:14 +0100

    Modified - Dialogos de inserir barco do jogador numa função separada do programa principal

--- Pedro Simões - Trabalho 3 - Batalha Naval.py
+++ Pedro Simões - Trabalho 3 - Batalha Naval.py
@@ -159,7 +159,7 @@ class Tabuleiro:
                 self.barcos.append
 
     def mostrar_tabuleiro(self):
-        print('  0 1 2 3 4 5 6 7 8 9')
+        print('\n  0 1 2 3 4 5 6 7 8 9')
         
         for i in range(10):
             linha_letra = chr(65 + i)
@@ -168,11 +168,11 @@ class Tabuleiro:
             print(f'{linha_letra} {linha_valores}')
 
 
-    # def colocar_barco(Barco):
+
 
     # def receber_tiro(coord):
 
-    # def mostrar(): (mostrar o tabuleiro)
+
 
 # Pode ser o jogador ou o computador
 class Jogador:
@@ -183,36 +183,87 @@ class Jogador:
 
     # def efetuar_tiro(): 
 
-"""
-print('  0 1 2 3 4 5 6 7 8 9\n'
-      'A ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n'
-      'B ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n'
-      'C ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n'
-      'D ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n'
-      'E ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n'
-      'F ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n'
-      'G ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n'
-      'H ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n'
-      'I ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n'
-      'J ~ ~ ~ ~ ~ ~ ~ ~ ~ ~')
-"""
-"""
-tabuleiro_jogador = [
-    ['null', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-    ['A', 'A0' 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']
-    ['B', 'B0' 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9']
-    ['C', 'C0' 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']
-    ['D', 'D0' 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9']
-    ['E', 'E0' 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9']
-    ['F', 'F0' 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9']
-    ['G', 'G0' 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9']
-    ['H', 'H0' 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9']
-    ['I', 'I0' 'I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9']
-    ['J', 'J0' 'J1', 'J2', 'J3', 'J4', 'J5', 'J6', 'J7', 'J8', 'J9']
-]
-"""
 
+def inserir_barcos_jogador():
+    tabuleiro_jogador.mostrar_tabuleiro()
+
+    print('\n--- Inserir Barcos ---')
+
+    # ---------------------------------------------------
+
+    print('\nPorta-avião (5 células):\n')
+
+    orientacao = str(input('Introduz a orientação ( H / V ): '))
+    posicao_inicial = str(input('Introduz a posição inicial: '))
+
+    porta_aviao1 = Barco(5, orientacao, posicao_inicial)
+
+    tabuleiro_jogador.colocar_barco(porta_aviao1)
+    tabuleiro_jogador.mostrar_tabuleiro()
+
+    # ---------------------------------------------------
+
+    print('\nCruzador (4 células):\n')
+
+    orientacao = str(input('Introduz a orientação ( H / V ): '))
+    posicao_inicial = str(input('Introduz a posição inicial: '))
+
+    cruzador1 = Barco(4, orientacao, posicao_inicial)
+
+    tabuleiro_jogador.colocar_barco(cruzador1)
+    tabuleiro_jogador.mostrar_tabuleiro()
+
+    # ---------------------------------------------------
+
+    print('\nContratorpedeiro (3 células):\n')
+
+    orientacao = str(input('Introduz a orientação ( H / V ): '))
+    posicao_inicial = str(input('Introduz a posição inicial: '))
+
+    contratorped1 = Barco(3, orientacao, posicao_inicial)
+
+    tabuleiro_jogador.colocar_barco(contratorped1)
+    tabuleiro_jogador.mostrar_tabuleiro()
+
+    # ---------------------------------------------------
+
+    print('\nContratorpedeiro (3 células):\n')
+
+    orientacao = str(input('Introduz a orientação ( H / V ): '))
+    posicao_inicial = str(input('Introduz a posição inicial: '))
+
+    contratorped2 = Barco(3, orientacao, posicao_inicial)
+
+    tabuleiro_jogador.colocar_barco(contratorped2)
+    tabuleiro_jogador.mostrar_tabuleiro()
+
+    # ---------------------------------------------------
+
+    print('\nSubmarino (2 células:)\n')
+
+    orientacao = str(input('Introduz a orientação ( H / V ): '))
+    posicao_inicial = str(input('Introduz a posição inicial: '))
+
+    submarino1 = Barco(2, orientacao, posicao_inicial)
+
+    tabuleiro_jogador.colocar_barco(submarino1)
+    tabuleiro_jogador.mostrar_tabuleiro()
+
+    # ---------------------------------------------------
+
+    print('\nSubmarino (2 células:)\n')
+
+    orientacao = str(input('Introduz a orientação ( H / V ): '))
+    posicao_inicial = str(input('Introduz a posição inicial: '))
+
+    submarino2 = Barco(2, orientacao, posicao_inicial)
+
+    tabuleiro_jogador.colocar_barco(submarino2)
+    tabuleiro_jogador.mostrar_tabuleiro()
+
+# --------------
 # INÍCIO AQUI
+# --------------
 
 tabuleiro_jogador = Tabuleiro()
 tabuleiro_maquina = Tabuleiro()
@@ -220,101 +271,7 @@ tabuleiro_maquina = Tabuleiro()
 print('\n/\ -- BATALHA NAVAL -- /\\\n')
 nome = input('O meu NOME: ')
 
+inserir_barcos_jogador()
+
 # jogador = Jogador(nome, tabuleiro1, barcos)
 
-print('\n--- Inserir Barcos ---')
-
-# ---------------------------------------------------
-
-print('\nPorta-avião (5 células):\n')
-
-orientacao = str(input('Introduz a orientação ( H / V ): '))
-posicao_inicial = str(input('Introduz a posição inicial: '))
-
-porta_aviao1 = Barco(5, orientacao, posicao_inicial)
-
-tabuleiro_jogador.colocar_barco(porta_aviao1)
-tabuleiro_jogador.mostrar_tabuleiro()
-
-# ---------------------------------------------------
-
-print('\nCruzador (4 células):\n')
-
-orientacao = str(input('Introduz a orientação ( H / V ): '))
-posicao_inicial = str(input('Introduz a posição inicial: '))
-
-cruzador1 = Barco(4, orientacao, posicao_inicial)
-
-tabuleiro_jogador.colocar_barco(cruzador1)
-tabuleiro_jogador.mostrar_tabuleiro()
-
-# ---------------------------------------------------
-
-print('\nContratorpedeiro (3 células):\n')
-
-orientacao = str(input('Introduz a orientação ( H / V ): '))
-posicao_inicial = str(input('Introduz a posição inicial: '))
-
-contratorped1 = Barco(3, orientacao, posicao_inicial)
-
-tabuleiro_jogador.colocar_barco(contratorped1)
-tabuleiro_jogador.mostrar_tabuleiro()
-
-# ---------------------------------------------------
-
-print('\nContratorpedeiro (3 células:\n)')
-
-orientacao = str(input('Introduz a orientação ( H / V ): '))
-posicao_inicial = str(input('Introduz a posição inicial: '))
-
-contratorped2 = Barco(3, orientacao, posicao_inicial)
-
-tabuleiro_jogador.colocar_barco(contratorped2)
-tabuleiro_jogador.mostrar_tabuleiro()
-
-# ---------------------------------------------------
-
-print('\nSubmarino (2 células:\n)')
-
-orientacao = str(input('Introduz a orientação ( H / V ): '))
-posicao_inicial = str(input('Introduz a posição inicial: '))
-
-submarino1 = Barco(2, orientacao, posicao_inicial)
-
-tabuleiro_jogador.colocar_barco(submarino1)
-tabuleiro_jogador.mostrar_tabuleiro()
-
-# ---------------------------------------------------
-
-print('\nSubmarino (2 células:\n)')
-
-orientacao = str(input('Introduz a orientação ( H / V ): '))
-posicao_inicial = str(input('Introduz a posição inicial: '))
-
-submarino2 = Barco(2, orientacao, posicao_inicial)
-
-tabuleiro_jogador.colocar_barco(submarino2)
-tabuleiro_jogador.mostrar_tabuleiro()
-
-
-
-print(porta_aviao1.coordenadas_ocupadas)
-
-# CONTINUAR AQUIIIIIIIIIIIIIIIIIIIII
-
-# cruzador1
-
-# contratorped1
-
-# contratorped2
-
-# submarino1
-
-# submarino2
-
-x = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-
-#print(x[0][2])
